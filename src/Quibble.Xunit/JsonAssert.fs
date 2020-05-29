792abf08d3b004b030802c5b8b5f803dcda3b822
Einar Høst <Einar.Host@nrk.no>
2020-05-29 14:07:37 +0200

    Drop examples project.

---
+++ src/Quibble.Xunit/JsonAssert.fs
@@ -0,0 +1,9 @@
+﻿namespace Quibble.Xunit
+
+module JsonAssert =
+    
+    let Equal (expectedJsonString: string, actualJsonString: string) : unit =
+        Assert.JsonEqual (expectedJsonString, actualJsonString)
+        
+    let EqualOverrideDefault (expectedJsonString: string, actualJsonString: string, diffConfig : JsonDiffConfig) : unit =
+        Assert.JsonEqualOverrideDefault (expectedJsonString, actualJsonString, diffConfig)
